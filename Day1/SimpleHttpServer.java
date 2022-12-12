f57272a4a9853ccb8a31817b526f650e1efd0aa9
nitis <nitish8152@gmail.com>
2022-12-12 08:28:47 +0530

    Handelr And Simple http

---
+++ Day1/SimpleHttpServer.java
@@ -0,0 +1,26 @@
+public class SimpleHttpServer {
+    public static int DEFAULT_PORT = 9000;
+    public static int port;
+    private HttpServer httpServer;
+
+    private void start(int port){
+        this.port = port;
+        try{
+            httpServer = HttpServer.create(new InetSocketAddress(port), 0);
+            System.out.println("server started at " + port);
+            httpServer.createContext("/", new Handlers.RootHandler());
+            httpServer.createContext("/echoHeader", new Handlers.EchoHeaderHandler());
+            httpServer.createContext("/echoGet", new Handlers.EchoGetHandler());
+            httpServer.createContext("/echoPost", new Handlers.EchoPostHandler());
+            httpServer.setExecution(null);
+            httpServer.start();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args){
+        SimpleHttpServer httpsServer = new SimpleHttpServer();
+        httpsServer.start(DEFAULT_PORT);
+    }
+}
